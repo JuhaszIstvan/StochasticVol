ce884069c00d5b864744ce168ba7c35de3751ac3
Istvan <istvan.juhasz@gmail.com>
2020-11-05 14:45:53 +0100

    gitignore fixed

--- thesis.py
+++ thesis.py
@@ -95,10 +95,12 @@ if __name__ == '__main__':
 
 
     #Call option price
-
-
     plt.show()
 
     #plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ), linewidth=2, color='r')
     #plt.show()
+
+
+
+
     sys.exit(0)
